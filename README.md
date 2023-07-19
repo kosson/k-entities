2847f10e0c8674c5e5ce96a1dd3c34c639d5d3cf
kosson <kosson@gmail.com>
2023-07-19 21:17:01 +0300

    API doc with swagger

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Aplicația are în țintă realizarea unui API cu ajutorul căruia să poți des
 
 Construcția se face pe Fastify din motive legate de viteză și oportunități de modularizare.
 
+## Instalare și pornire
+
+Clonează repo-ul și apoi `npm install`.
+Pornește-l cu `nodemon server.js`.
+## Documentarea API-ului
+
+Aplicația are instalat swagger și swagger-ui de la Fastify cu care se creează automat documentația API-ului.
+După ce pornești serverul, consultă: http://127.0.0.1:3300/documentation/static/index.html. Rulează doar ruta de test, deocamdată. Vezi mai jos în imagine:
+
+![](docs/img/swagger-test-route.png)
+
 ## Resurse pentru dezvoltare
 
 [Building a modular monolith with Fastify - Matteo Collina, Node Congress 2023](https://www.youtube.com/watch?v=e1jkA-ee_aY)
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Importă frameworkul Fastify și creează o instanță
 import Fastify from 'fastify';
 import autoload from '@fastify/autoload';
-import multipart from '@fastify/multipart';
-import websocket from '@fastify/websocket';
+import swagger from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
+import {SwaggerTheme} from 'swagger-themes';
 
 import { dirname, filename, join } from 'desm';
 import validator from 'validator';
-import dotenv from 'dotenv';
-dotenv.config();
 
 // Node specific
 import * as fs from 'node:fs'
@@ -15,20 +17,6 @@ import util from 'util';
 import { pipeline } from 'stream';
 const pump = util.promisify(pipeline);
 import PDFParser from "pdf2json";
-
-// instanțiază nodul
-import { knode } from './node.js';
-import {unixfs} from '@helia/unixfs';
-const nixfs = unixfs(knode);        // creează un sistem de fișiere suport pentru nodul Helia, care în acest caz este UnixFS
-const encoder = new TextEncoder();  // folosește TextEncoder pentru a transforma string-urile în Uint8Arrays
-const decoder = new TextDecoder();  // this decoder will turn Uint8Arrays into strings
-
-// CONECTARE LA NODUL KUBO
-import { create, globSource, urlSource } from 'kubo-rpc-client';
-// connect to the default API address http://localhost:5001
-const kuboClient = await create();
-
-
 
 // Obiectul pentru configurarea logging-ului
 const envToLogger = {
@@ -59,6 +47,15 @@ const opts = {
 // cu autoload se încarcă automat toate pluginurile și toate rutele
 export async function build (opts) {
   const app = Fastify(opts);
+
+  app.register(swagger);
+  app.register(swaggerUI, {
+    theme: {
+      css: [
+        {filename: 'theme.css', content: new SwaggerTheme('v3').getBuffer('dark')}
+      ]
+    }
+  });
 
   app.register(autoload, {
     dir: join(import.meta.url, 'plugins'),
